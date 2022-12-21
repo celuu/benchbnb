1d923583a6e23ef72e42323fd6aed605687c74ff
celuu <christine.e.luu@gmail.com>
2022-12-21 10:34:28 -0800

    Getting project set up

--- config/application.rb
+++ config/application.rb
@@ -18,13 +18,13 @@ require "action_view/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module AuthenticateMe
+module BenchBnb
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore,
-      key: '_auth_me_session',
+      key: '_bench_bnb_session',
       same_site: :lax, 
       secure: Rails.env.production?
 
